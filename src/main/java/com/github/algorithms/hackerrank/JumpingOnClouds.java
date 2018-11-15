ecf9724657166a142dd5c23ed19e88fd092c92cb
Razvan Faragau <Razvan.Faragau@paddypowerbetfair.com>
2018-11-15 15:17:48 +0200

    Added solution for JumpingOnClouds algorithm - https://www.hackerrank.com/challenges/jumping-on-the-clouds/.

---
+++ src/main/java/com/github/algorithms/hackerrank/JumpingOnClouds.java
@@ -0,0 +1,24 @@
+package com.github.algorithms.hackerrank;
+
+public class JumpingOnClouds {
+    public static void main(String[] args) {
+        //3
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 0, 1, 0}));
+        //3
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 0, 1, 0, 0}));
+        //4
+        System.out.println(jumpingOnClouds(new int[]{0, 0, 1, 0, 0, 1, 0}));
+    }
+
+    static int jumpingOnClouds(int[] c) {
+        int jumps = 0;
+
+        int cloudIndex = 0;
+        while (cloudIndex < c.length - 2) {
+            cloudIndex += (c[cloudIndex + 2] % 2 == 0) ? 2 : 1;
+            jumps++;
+        }
+
+        return cloudIndex != c.length - 1 ? ++jumps : jumps;
+    }
+}

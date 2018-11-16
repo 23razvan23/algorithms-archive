4689234fd4d828297892c71fc3666fd9fd99f336
Razvan Faragau <Razvan.Faragau@paddypowerbetfair.com>
2018-11-16 16:29:00 +0200

    Added solution for the Utopian Tree problem - https://www.hackerrank.com/challenges/utopian-tree/.

---
+++ src/main/java/com/github/algorithms/hackerrank/UtopianTree.java
@@ -0,0 +1,30 @@
+package com.github.algorithms.hackerrank;
+
+public class UtopianTree {
+    public static void main(String[] args) {
+        //1
+        System.out.println(utopianTree(0));
+        //2
+        System.out.println(utopianTree(1));
+        //7
+        System.out.println(utopianTree(4));
+        //6
+        System.out.println(utopianTree(3));
+    }
+
+    // Complete the utopianTree function below.
+    static int utopianTree(int n) {
+        int period = 0;
+        int h = 1;
+
+        while (period++ < n) {
+            if (period % 2 == 0) {
+                h++;
+            } else {
+                h *= 2;
+            }
+        }
+
+        return h;
+    }
+}
